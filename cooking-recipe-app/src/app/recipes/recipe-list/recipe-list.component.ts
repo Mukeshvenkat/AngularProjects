d98bb84d176f18a48be169982b6cb690cd8ced45
MukeshVenkat <mukiv0802@gmail.com>
2021-12-23 20:49:51 +0530

    Recipe Model added & sample recipe list is added.

--- cooking-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cooking-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Recipe Description', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-oreo-truffles-078-1544222424.jpg')
+  ]
 
   constructor() { }
 
