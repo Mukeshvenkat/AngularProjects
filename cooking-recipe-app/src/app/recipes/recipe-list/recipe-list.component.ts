af7f8c07e0552a322e783ea908859112d1bccd7f
MukeshVenkat <mukiv0802@gmail.com>
2021-12-26 09:25:14 +0530

    Data exchange between recipe components are added.

--- cooking-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cooking-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipes.model';
 
 @Component({
@@ -8,12 +8,17 @@ import { Recipe } from '../recipes.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Test Recipe Description', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-oreo-truffles-078-1544222424.jpg')
+    new Recipe('Test Recipe', 'Test Recipe Description', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-oreo-truffles-078-1544222424.jpg'),
+    new Recipe('Test Recipe 1', 'Test Recipe Description - New', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-oreo-truffles-078-1544222424.jpg')
   ]
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
