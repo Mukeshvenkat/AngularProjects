1b637e49b46e23995ff2b6f250e119114102b4a9
MukeshVenkat <mukiv0802@gmail.com>
2022-01-05 01:39:23 +0530

    routing with passing id as params added.

--- cooking-recipe-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ cooking-recipe-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipes.model';
@@ -14,13 +13,18 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private shoppinglistService: ShoppingListService, private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private shoppinglistService: ShoppingListService, private recipeService: RecipeService, private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {  
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.recipe = this.recipeService.getRecipe(this.id);
     });
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
   addToShoppingList(){  
