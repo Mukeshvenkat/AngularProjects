90ad26457957e7975042b2b26a08e23d3399aab0
MukeshVenkat <mukiv0802@gmail.com>
2022-01-12 21:55:27 +0530

    Reactive approach delete ingredient in edit/add recipe added.

--- cooking-recipe-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ cooking-recipe-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -77,6 +77,10 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  onDeleteIngredient(index: number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index)
+  }
+
   get controls() { // a getter!
     let formArray = (<FormArray>this.recipeForm.get('ingredients')); 
     return formArray.controls;
