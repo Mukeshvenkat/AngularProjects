e6b00de4f2499283345fb5166a560c86f46a83b7
MukeshVenkat <mukiv0802@gmail.com>
2022-01-14 23:47:45 +0530

    Loading Spinner added, SignUp with error handling is completed.

--- cooking-recipe-app/src/app/auth/auth.service.ts
+++ cooking-recipe-app/src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 interface AuthResponseData{
     idToken: string;
@@ -21,6 +23,17 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        )
+        ).pipe(catchError(errorRes => {
+            let errorMessage = "An Unkown error has occured!";
+            if (!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage);
+            }
+            switch (errorRes.error.error.message) {
+                case "EMAIL_EXISTS": {
+                    errorMessage = "This email is already in use";
+                }
+            }
+            return throwError(errorMessage);
+        }));
     }
 }
