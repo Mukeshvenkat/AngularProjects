8d76cda043c08443c240c177f9fb79b0dd77ee55
MukeshVenkat <mukiv0802@gmail.com>
2022-01-17 23:37:59 +0530

    Lazy Loading implemented & unwanted imports removed.

--- cooking-recipe-app/src/app/app-routing.module.ts
+++ cooking-recipe-app/src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: '/recipes', pathMatch: 'full'}
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { 
+        path: 'recipes', 
+        loadChildren: () => import('./recipes/recipes.module').then(moduleClass => moduleClass.RecipesModule)
+    },
+    { 
+        path: 'shopping-list', 
+        loadChildren: () => import('./shopping-list/shopping-list.module').then(moduleClass => moduleClass.ShoppingListModule)
+    },
+    { 
+        path: 'auth', 
+        loadChildren: () => import('./auth/auth.module').then(moduleClass => moduleClass.AuthModule)
+    }
 ]
 
 @NgModule({
--- cooking-recipe-app/src/app/auth/auth.module.ts
+++ cooking-recipe-app/src/app/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { AuthComponent } from "./auth.component";
 @NgModule({
     declarations: [AuthComponent],
     imports: [ SharedModule, FormsModule, RouterModule.forChild([
-        {path: 'auth', component: AuthComponent }
+        {path: '', component: AuthComponent }
     ])]
 })
 
