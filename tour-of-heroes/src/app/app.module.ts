f6ebb49cfe689e86ce1f390e8bbd940ee3773f2f
MukeshVenkat <mukiv0802@gmail.com>
2022-01-24 23:45:41 +0530

    Header component & Heroes Feature all components added.

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { TopHeroesComponent } from './heroes/top-heroes/top-heroes.component';
+import { HeroesDetailsComponent } from './heroes/heroes-details/heroes-details.component';
+import { HeroesListComponent } from './heroes/heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroesComponent,
+    TopHeroesComponent,
+    HeroesDetailsComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule
